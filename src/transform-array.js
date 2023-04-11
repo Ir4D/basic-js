205a348f18cacaecdc76da36da2ff02e830ee236
Ir4D <iradedova@gmail.com>
2023-04-12 01:09:43 +0300

    feat: add few tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!(arr[i]) || arr[i] === undefined) {
+      continue;
+    } else if (arr[i] == '--discard-next') {
+      i++;
+      i++;
+    } else if (arr[i] == '--discard-prev') {
+      if (arr[i-1] != undefined) {
+        result.pop();
+      }
+    } else if (arr[i] == '--double-next') {
+      if (arr[i+1] != undefined) {
+        result.push(arr[i+1]);
+      }
+    } else if (arr[i] == '--double-prev') {
+      if (arr[i-1] != undefined) {
+        result.push(arr[i-1]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error("Invalid date!");
+  }
+  if (date.hasOwnProperty('toString')) {
+    throw new Error("Invalid date!");
+  }
+  if (date instanceof Date && !isNaN(date)) {
+    const seasons = {
+      'spring': [2, 3, 4], 
+      'summer': [5, 6, 7], 
+      'autumn': [8, 9, 10], 
+      'winter': [0, 1, 11]
+    };
+    let month = date.getMonth();
+    let result;
+    for (let key in seasons) {
+      for (let i = 0; i < 3; i++) {
+        if (seasons[key][i] === month) {
+          result = key;
+        }
+      }
+    }
+    return result;
+  } else {
+    throw new Error("Invalid date!");
+  }
 }
 
 module.exports = {
