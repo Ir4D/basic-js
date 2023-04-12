466e905de137e94ef03aff38476be8d37a16728b
Ir4D <iradedova@gmail.com>
2023-04-12 23:58:27 +0300

    feat: add extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let add = [];
+  let arr = [];
+  str = String(str);
+  if (options["addition"] !== undefined) {
+    options.addition = String(options.addition);
+  }
+  if (options["repeatTimes"] == undefined) {
+    options["repeatTimes"] = 1;
+  }
+  if (options["additionRepeatTimes"] == undefined) {
+    options["additionRepeatTimes"] = 1;
+  }
+  for (let i = 0; i < options.repeatTimes; i++) {
+    add = [];
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      add.push(options.addition);
+    }
+    if (options["additionSeparator"] !== undefined) {
+      arr.push(str + add.join(options.additionSeparator));
+    } else {
+      arr.push(str + add.join('|'));
+    }
+  }
+  if (options["separator"] !== undefined) {
+    return arr.join(options.separator);
+  } else {
+    return arr.join('+');
+  }
 }
 
 module.exports = {
