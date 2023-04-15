833a7a29249a94a7bcd9ecdb1989c62604c4d75c
Ir4D <iradedova@gmail.com>
2023-04-15 12:55:18 +0300

    feat: add dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let arr = [];
+  domains.forEach((item) => {
+    let element = item.split('.');
+    let newElement = '';
+    for (let i = element.length - 1; i >= 0; i--) {
+      newElement += '.' + element[i];
+      arr.push(newElement);
+    }
+  })
+
+  const counts = {};
+  arr.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+  return counts;
 }
 
 module.exports = {
