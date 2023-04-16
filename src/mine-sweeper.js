77554f5549065a697ddf35a097ec5669160a29a0
Ir4D <iradedova@gmail.com>
2023-04-17 01:13:33 +0300

    feat: add mine sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let newArr = [];
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    let minArr = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        if (matrix[i][j+1] === true) {
+          count++;
+        }
+        if (matrix[i][j-1] === true) {
+          count++;
+        }
+        if (matrix[i+1][j-1] === true) {
+          count++;
+        }
+        if (matrix[i+1][j] === true) {
+          count++;
+        }
+        if (matrix[i+1][j+1] === true) {
+          count++;
+        }
+      } else if (i === matrix.length - 1) {
+        if (matrix[i][j+1] === true) {
+          count++;
+        }
+        if (matrix[i][j-1] === true) {
+          count++;
+        }
+        if (matrix[i-1][j-1] === true) {
+          count++;
+        }
+        if (matrix[i-1][j] === true) {
+          count++;
+        }
+        if (matrix[i-1][j+1] === true) {
+          count++;
+        }
+      } else {
+        if (matrix[i][j+1] === true) {
+          count++;
+        }
+        if (matrix[i][j-1] === true) {
+          count++;
+        }
+        if (matrix[i-1][j-1] === true) {
+          count++;
+        }
+        if (matrix[i-1][j] === true) {
+          count++;
+        }
+        if (matrix[i-1][j+1] === true) {
+          count++;
+        }
+        if (matrix[i+1][j-1] === true) {
+          count++;
+        }
+        if (matrix[i+1][j] === true) {
+          count++;
+        }
+        if (matrix[i+1][j+1] === true) {
+          count++;
+        }
+      }
+      minArr.push(count);
+      count = 0;
+    }
+    newArr.push(minArr);
+  }
+  return newArr;
 }
 
 module.exports = {
